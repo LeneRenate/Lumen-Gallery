bd956be7e6a174d08bd7b84a3ae1ea5bb55dd177
LeneRenate <Lene.renate.rosseboe@gmail.com>
2026-02-03 14:00:45 +0100

    Fix imports

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,17 @@
-/**
-NAV: Home - Exhibition - Visit -Contact - About
-**/
-
 import { Link } from "react-router-dom";
 import styles from "../styles/component style/Header.module.css";
 
 export default function Header() {
   return (
     <>
-      <header>
+      <header className={`flex flex-row justify-evenly`}>
         <Link to="/" className="brandLogo">
           <img className="LumenIcon" src="" alt="logo" />
           <img className="LumenName" src="" alt="LUMEN" />
         </Link>
         <div className={`flex flex-col justify-evenly`}>
           <div className={`flex flex-row justify-end ${styles.upperRow}`}>
-            <label className={`{styles.themeToggle}`}>
+            <label className={`${styles.themeToggle}`}>
               <input type="checkbox" className={`${styles.themeToggleInput}`} />
               <span className={`${styles.themeToggleSlider}`} />
             </label>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,23 +1,39 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import HomePage from "../pages/HomePage";
+import ExhibitionPage from "../pages/ExhibitionPage";
+import AboutPage from "../pages/AboutPage";
+import VisitPage from "../pages/VisitPage";
+import ContactPage from "../pages/ContactPage";
+import ArtistPage from "../pages/ArtistPage";
+import NewsAndEventsPage from "../pages/NewsAndEventsPage";
+import ExhibitionsArchivePage from "../pages/ExhibitionsArchivePage";
+import PressPage from "../pages/PressPage";
+import SearchResultsPage from "../pages/SearchResultsPage";
+import ErrorPage from "../pages/ErrorPage";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <h1>An error occured</h1>,
+      children: [
+        { index: true, element: <HomePage /> },
+        { path: "whats-on", element: <ExhibitionPage /> },
+        { path: "about", element: <AboutPage /> },
+        { path: "visit", element: <VisitPage /> },
+        { path: "contact", element: <ContactPage /> },
+        { path: "artist/:id", element: <ArtistPage /> },
+        { path: "news-and-events", element: <NewsAndEventsPage /> },
+        { path: "archive", element: <ExhibitionsArchivePage /> },
+        { path: "press", element: <PressPage /> },
+        { path: "search", element: <SearchResultsPage /> },
+        { path: "*", element: <ErrorPage /> },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    errorElement: <h1>An error occured</h1>,
-    children: [
-      { index: true, element: <HomePage /> },
-      { path: "whats-on", element: <ExhibitionPage /> },
-      { path: "about", element: <AboutPage /> },
-      { path: "visit", element: <VisitPage /> },
-      { path: "contact", element: <ContactPage /> },
-      { path: "artist/:id", element: <ArtistPage /> },
-      { path: "news-and-events", element: <NewsAndEventsPage /> },
-      { path: "archive", element: <ExhibitionsArchivePage /> },
-      { path: "press", element: <PressPage /> },
-      { path: "search", element: <SearchResultsPage /> },
-      { path: "*", element: <ErrorPage /> },
-    ],
+    basename: "/Lumen-Gallery/",
   },
-]);
+);
