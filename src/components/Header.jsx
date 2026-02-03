bd84d7632e759d908cbcb26aa597995f2fe99432
LeneRenate <Lene.renate.rosseboe@gmail.com>
2026-02-03 13:42:19 +0100

    Add skeleton and base-style to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,52 @@
 /**
-NAV: Home - Exhibition - Visit - About
+NAV: Home - Exhibition - Visit -Contact - About
 **/
+
+import { Link } from "react-router-dom";
+import styles from "../styles/component style/Header.module.css";
+
 export default function Header() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <Link to="/" className="brandLogo">
+          <img className="LumenIcon" src="" alt="logo" />
+          <img className="LumenName" src="" alt="LUMEN" />
+        </Link>
+        <div className={`flex flex-col justify-evenly`}>
+          <div className={`flex flex-row justify-end ${styles.upperRow}`}>
+            <label className={`{styles.themeToggle}`}>
+              <input type="checkbox" className={`${styles.themeToggleInput}`} />
+              <span className={`${styles.themeToggleSlider}`} />
+            </label>
+            {/* <label className={`${styles.languageSelect}`}>
+              <input type="" name="" id="" />
+            </label> */}
+          </div>
+          <div className={`${styles.lowerRow}`}>
+            <nav className={`${styles.navbar}`}>
+              <ul className={`flex flex-row ${styles.navList}`}>
+                <li className={`${styles.listElement}`}>
+                  <Link to="/">Home</Link>
+                </li>
+                <li className={`${styles.listElement}`}>
+                  <Link to="whats-on">What's on</Link>
+                </li>
+                <li className={`${styles.listElement}`}>
+                  <Link to="visit">Visit</Link>
+                </li>
+                <li className={`${styles.listElement}`}>
+                  <Link to="contact">Contact Us</Link>
+                </li>
+                <li className={`${styles.listElement}`}>
+                  <Link to="about">About</Link>
+                </li>
+                {/* "Us" on contact..? */}
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -7,13 +7,17 @@ export const router = createBrowserRouter([
     element: <App />,
     errorElement: <h1>An error occured</h1>,
     children: [
-      {index: true, element: },
-      {path: "contactpage", element: ContactPage},
-      {path: "art/:artId", element: ArtPage},
-      {path: "artist/:artistId", element: ArtistPage},
-      {path: "searchresult", element: SearchResultPage},
-{path: }
-      {path: "*", element: ErrorPage}
-    ]
+      { index: true, element: <HomePage /> },
+      { path: "whats-on", element: <ExhibitionPage /> },
+      { path: "about", element: <AboutPage /> },
+      { path: "visit", element: <VisitPage /> },
+      { path: "contact", element: <ContactPage /> },
+      { path: "artist/:id", element: <ArtistPage /> },
+      { path: "news-and-events", element: <NewsAndEventsPage /> },
+      { path: "archive", element: <ExhibitionsArchivePage /> },
+      { path: "press", element: <PressPage /> },
+      { path: "search", element: <SearchResultsPage /> },
+      { path: "*", element: <ErrorPage /> },
+    ],
   },
 ]);
